76ab1da61e55bf7abbf9f32fb94500023390371f
Anushka-0703 <anushka.970747@gmail.com>
2026-06-12 20:27:27 +0545

    final touch

--- calc.js
+++ calc.js
@@ -1,23 +1,26 @@
-const result = document.getElementById("inputBox");
-const clearBtn = document.getElementById("clear");
-const deleteBtn = document.getElementById("delete");
-const percentageBtn = document.getElementById("percentage");
-const divideBtn = document.getElementById("divide");
-const multiplyBtn = document.getElementById("multiply");
-const decimalBtn = document.getElementById("decimal");
-const equalBtn = document.getElementById("equal");
-const additionBtn = document.getElementById("addition");
-const subtractionBtn = document.getElementById("subtraction");
-const numberBtns = document.querySelectorAll(".number");
+let display = document.querySelector("input");
+let buttons = document.querySelectorAll("button");
 
-//Function to append number
-const appendNumber = (number) => {
+let string = "";
 
-};
-
-//Add eventListener to number buttons
-numberBtns.forEach((button) => {
-     button.addEventListener("click", () => {
-          appendNumber(button.innerText);
+buttons.forEach((button) => {
+     button.addEventListener("click", (e) => {
+          if (e.target.innerHTML === "=") {
+               try {
+                    string = eval(string);
+                    display.value = string;
+               } catch {
+                    display.value = "Error";
+               }
+          } else if (e.target.innerHTML === "AC") {
+               string = "";
+               display.value = string;
+          } else if (e.target.innerHTML === "DEL") {
+               string = string.slice(0, -1);
+               display.value = string;
+          } else {
+               string += e.target.innerHTML;
+               display.value = string;
+          }
      });
 });
